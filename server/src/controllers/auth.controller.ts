1bf5ae3564eacca2cd67874c274f63588ecd6826
Bryan <brainalexis119@gmail.com>
2026-03-14 22:12:59 -0600

    logging in and auth middleware

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,12 +3,13 @@ import { pool } from "../db/pool.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { resourceLimits } from "node:worker_threads";
+import { error } from "node:console";
 const SALT_ROUNDS = 12;
 const JWT_SECRET = process.env.JWT_SECRET;
 export async function register(req: Request, res: Response) {
   const { email, password } = req.body;
 
-  if (!email || password) {
+  if (!email || !password) {
     return response
       .status(400)
       .json({ error: "Email and Password are required" });
@@ -35,4 +36,43 @@ export async function register(req: Request, res: Response) {
   });
 }
 
-export async function login(req: Request, res: Response) {}
+export async function login(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  if (email! || password!) {
+    return res.status(400).json({ error: "Email and Password required" });
+  }
+
+  const result = await pool.query(
+    "SELECT id, password_hash FROM users WHERE email = $1",
+    [email],
+  );
+
+  if (result.rows.length === 0) {
+    return res.status(401).json({ error: "Email or password invalid" });
+  }
+
+  const user = result.rows[0];
+
+  const isvalid = bcrypt.compare(password, user.password_hash);
+
+  if (!isvalid) {
+    return res.status(401).json({
+      error: "Email or password invalid",
+    });
+  }
+
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not set in environment variables");
+  }
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "7d" });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  return res.json({ message: "Logged in successfully" });
+}
---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { Request, response, Response, NextFunction } from "express";
+import { error } from "node:console";
+import { json } from "node:stream/consumers";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export async function verify_user(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = req.cookies.token;
+  if (!token) {
+    return response.status(401).json({ error: "user isn't logged in" });
+  }
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not set in environment variables");
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+
+    (req as any).userId = decoded.userId;
+
+    next();
+  } catch {
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
+}
